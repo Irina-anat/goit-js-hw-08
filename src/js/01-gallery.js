bdc85c9efe054aa14b8b08c7622e9ea186869cb0
Iryna Rozmaita <iriszechka@ukr.net>
2023-04-05 18:28:54 +0300

    +1-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
+// імпорт SimpleLightbox згідно документації бібліотеки
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+// імпорт стилів
+const gallery = document.querySelector(`.gallery`);
+console.log(gallery);
 
+function markupGallery(galleryItems) {
+   
+    const markup = galleryItems.map(({ preview, original, description }) => 
+`<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join(""); 
+  return markup; 
+};
+
+gallery.innerHTML = markupGallery(galleryItems); 
+// бібліотека
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+console.log(lightbox);
 console.log(galleryItems);
