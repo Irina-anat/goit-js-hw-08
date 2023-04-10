31acc0d6b9f8bf04655915dda1173671db0c1630
Iryna Rozmaita <iriszechka@ukr.net>
2023-04-10 12:34:50 +0300

    2-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
 
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const STORAGE_KEY = "videoplayer-current-time";
+//Зберігаю час відтворення у локальне сховище. Ключем для сховища буде рядок "videoplayer-current-time".
+
+const iframe = document.querySelector('iframe');
+console.log(iframe)
+
+const player = new Player(iframe);//ініціалізую плеєр
+
+const onPlay = function (data) {
+    console.dir(data)
+    console.log(data.seconds)
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+    //Зберігаю час відтворення у локальне сховище      
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+//відслідковую подію timeupdate 
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)) || 0);
+//Під час перезавантаження сторінки скористалась методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+
+
+/*HTML містить <iframe> з відео для Vimeo плеєра. Напиши скрипт, який буде зберігати поточний час відтворення відео у локальне сховище і, після перезавантаження сторінки, продовжувати відтворювати відео з цього часу.*/
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,9 +11,11 @@ form.addEventListener(`submit`, onSubmit);
 let LOCALSTORAGE_KEY = "feedback-form-state";
 
 let dataForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+//перетв json на значення js LOCALSTORAGE_KEY
+
 const { email, message } = form.elements;
 //console.log(form.elements) [input, textarea, button, email: input, message: textarea]
-downloadPage();// виклик ф-ї для перевірки: Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+
 
 function onInput() {
   dataForm = { email: email.value, message: message.value };
@@ -23,23 +25,29 @@ function onInput() {
    console.log(localStorage)
 }
 
+//Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
 function downloadPage() {
   if (dataForm) {
     email.value = dataForm.email || ``;
-    console.log(dataForm.email)
+  //  console.log(dataForm.email)
     message.value = dataForm.message || ``;
-    console.log(dataForm.message)
+  //  console.log(dataForm.message)
   }  
 }
+downloadPage();
 
 //Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
 function onSubmit(e) {
   e.preventDefault();
   console.log({ email: email.value, message: message.value });
+
+  if (email.value === '' || message.value === '') {
+        return alert('Всі поля повинні бути заповнені')
+  }
+
   localStorage.removeItem(LOCALSTORAGE_KEY);
   e.currentTarget.reset();
-  dataForm = {};
-  console.log(dataForm)
+  
 }
 
 
